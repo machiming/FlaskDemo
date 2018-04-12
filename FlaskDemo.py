3ce67b6909e8b6d3277cb9dd035e3054308f8c33
837944832@qq.com <mcm10011121>
2018-04-12 17:57:07 +0800

    post get demo

--- FlaskDemo.py
+++ FlaskDemo.py
@@ -1,22 +1,34 @@
-from flask import Flask, json
+from flask import Flask, json, request
 
 import query
 
 app = Flask(__name__)
 
 
-@app.route('/<start>/<end>')
+@app.route('/')
+def a():
+    return '请传值'
+
+#post get请求
+@app.route('/gp', methods=["get","post"])
 def hello_world(start, end):
-    sql = "select * from mobile limit " + start + "," + end
+   if request.method=="get":
+       print(request.args['user'])
+   elif request.method=="post":
+       print(request.form['user'])
+
+    #sql查询
+   sql = "select * from mobile limit 0,5"
+   result = query.querysVal(sql)
+   return json.dumps(result, ensure_ascii=False)
+
+#传参函数
+@app.route('/phone/<phoneN>')
+def checkPhone(phoneN):
+    sql = "select * from mobile where phone=" + phoneN[0:7]
     result = query.querysVal(sql)
     return json.dumps(result, ensure_ascii=False)
 
-@app.route('/phone/<phoneN>')
-def checkPhone(phoneN):
-    sql = "select * from mobile where phone="+phoneN[0:7]
-    print(phoneN[0:7])
-    result = query.querysVal(sql)
-    return json.dumps(result, ensure_ascii=False)
 
 @app.route('/login')
 def login():
